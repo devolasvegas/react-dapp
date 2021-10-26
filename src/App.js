9a381418149537965b181a069ce8d683e41ad665
Devon Daviau <devondaviau@yahoo.ca>
2021-10-26 08:53:46 -0400

    start updating app with functions for interacting with the contract and metamask

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ethers } from "ethers";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import logo from "./logo.svg";
+import "./App.css";
+
+const greeterAddress = "";
 
 function App() {
+  const [greeting, setGreetingValue] = useState();
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+
+      try {
+        const data = await contract.greet();
+        console.log("data", data);
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
